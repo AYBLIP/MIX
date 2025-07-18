4d1140caccd731858d98c7b1464019ff7435f10e
AYBLIP <119825497+AYBLIP@users.noreply.github.com>
2025-07-19 04:30:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ optimizer_options = ['Adam', 'SGD', 'RMSprop']
 optimizer_choice = st.selectbox("Optimizer", optimizer_options)
 
 # Path model
-model_path = f'best_model_EfficientNet_{optimizer_choice}.h5'
+model_path = f'best_model_EfficientNetB0_{optimizer_choice}.h5'
 
 # Muat model
 try:
