051aa7b06e1165847e00131571615f96ffe2f043
AYBLIP <119825497+AYBLIP@users.noreply.github.com>
2025-07-19 09:27:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,30 +22,40 @@ optimizer_options = ['Adam', 'SGD', 'RMSprop']
 optimizer_choice = st.selectbox("Optimizer", optimizer_options)
 
 # Tentukan path model berdasarkan pilihan
-if model_choice == 'NASNetMobile' and optimizer_choice == 'Adam':
-    gdrive_link = 'https://drive.google.com/file/d/FILE_ID/view?usp=sharing'  # Ganti dengan link nyata
-    # Ekstrak file ID dari link
-    file_id = gdrive_link.split('/d/')[1].split('/')[0]
-    # Nama file lokal
-    model_filename = 'NASNetMobile_Adam.keras'
-    # Unduh file dari Google Drive
-    gdown.download(f'https://drive.google.com/uc?id={file_id}', model_filename, quiet=False)
-    model_path = model_filename
-else:
-    if model_choice in ['MobileNetV2', 'NASNetMobile']:
-        model_path = f'best_model_{model_choice}_{optimizer_choice}.h5'
-    else:
-        if model_choice in ['EfficientNetB0']:
-            model_path = f'best_model_{model_choice}_{optimizer_choice}.h5'
-            model = tf.keras.models.load_model(
-                model_path,
-                custom_objects={
-                    'swish': swish,
-                    'FixedDropout': FixedDropout
-                }
-            )
-        st.success(f"Model {model_choice} dengan optimizer {optimizer_choice} berhasil dimuat.")
+import gdown
+import tensorflow as tf
 
+# Pastikan fungsi dan variabel yang digunakan seperti swish dan FixedDropout sudah didefinisikan sebelumnya
+
+try:
+    if model_choice == 'NASNetMobile' and optimizer_choice == 'Adam':
+        gdrive_link = 'https://drive.google.com/file/d/FILE_ID/view?usp=sharing'  # Ganti dengan link nyata
+        # Ekstrak file ID dari link
+        file_id = gdrive_link.split('/d/')[1].split('/')[0]
+        # Nama file lokal
+        model_filename = 'NASNetMobile_Adam.keras'
+        # Unduh file dari Google Drive
+        gdown.download(f'https://drive.google.com/uc?id={file_id}', model_filename, quiet=False)
+        model_path = model_filename
+    else:
+        # Untuk model lain
+        if model_choice in ['MobileNetV2', 'NASNetMobile', 'EfficientNetB0']:
+            model_path = f'best_model_{model_choice}_{optimizer_choice}.h5'
+            # Jika model adalah EfficientNetB0, muat model dengan custom_objects
+            if model_choice == 'EfficientNetB0':
+                model = tf.keras.models.load_model(
+                    model_path,
+                    custom_objects={
+                        'swish': swish,
+                        'FixedDropout': FixedDropout
+                    }
+                )
+            else:
+                # Untuk model lain tanpa custom_objects
+                model = tf.keras.models.load_model(model_path)
+            st.success(f"Model {model_choice} dengan optimizer {optimizer_choice} berhasil dimuat.")
+except Exception as e:
+    st.error(f"Gagal memuat model: {str(e)}")
 # Daftar kelas
 kelas = ['Kue Dadar Gulung', 'Kue Kastengel', 'Kue Klepon', 'Kue Lapis', 'Kue Lumpur', 'Kue Putri Salju', 'Kue Risoles', 'Kue Serabi']
 
